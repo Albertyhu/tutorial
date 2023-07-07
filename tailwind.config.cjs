df6804b03b03abce23fe8166e1f3c298d910fbdf
Albertyhu <hualbert@rocketmail.com>
2023-07-06 19:46:09 -0700

    added blogs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        xm: { min: "360px" },
+        sm: { min: "636px" },
+        md: { min: "768px" },
+        "md-2": { min: "850px" },
+        lg: { min: "1024px" },
+        xl: { min: "1280px" },
+        "2xl": { min: "1536px" },
+      },
+    },
+  },
+  plugins: [],
+};
