7de53127e9353915107c8d99eda2eb0751bbc15d
Albertyhu <hualbert@rocketmail.com>
2023-07-25 02:31:05 -0700

    added design initiative parallax

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,26 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 import preact from "@astrojs/preact";
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), preact()],
+  integrations: [
+    react(), 
+    tailwind(), 
+    preact(),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        collections: [
+          // Content collections
+        ],
+      },
+    }),
+  ],
     site: 'https://Albertyhu.github.io/tutorial',
   base: '/',
 });
